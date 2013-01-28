e6e78cf7814b55ca6186d57dfd5df755e5b19156
Roger Qiu <roger.qiu@polycademy.com>
2013-01-29 02:09:39 +1100

    initiate

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+cmcdragonkai:$1$cpuKoPXh$U6SrJxibRWdc38GQczzXD1:Roger Qiu:roger.qiu@polycademy.com:admin,user
