29722bb5aa5b495a8b0049847438604117edbd9f
Roger Qiu <roger.qiu@polycademy.com>
2013-02-08 15:09:46 +1100

    Pulled in remote changes and added into the git.

--- conf/users.auth.php
+++ conf/users.auth.php
@@ -11,3 +11,7 @@
 
 cmcdragonkai:$1$cpuKoPXh$U6SrJxibRWdc38GQczzXD1:Roger Qiu:roger.qiu@polycademy.com:admin,user
 mariafoo:$2a$08$lsesZL7v2JssTsF8taTK9.xGazDDnVTHlk81CKZxiQ5FXcXS.QKOW:Maria Foo:mariafoo@iinet.net.au:user
+mattmaher:$2a$08$y0aqzsdYSbXXfLaxje1JWuRloeJ./spbSx.lLokjCThHg6uRDar/S:Matthew Maher:matthew.alexander.maher@gmail.com:user
+phuonglantrinh:$2a$08$Z42YaUv0AyjySM2IaQIgB.njBscNsxGqN1QdVrMY.5X/pJdWbWKDy:Phuong Lan Trinh:phuonglan.trinh91@gmail.com:user
+ash:$2a$08$X7dw1TKHvEENBHsgL62L0e1MjeYfbPiFpYuCOKKotT2W73.1XA9ny:Ash Newport:chickensoup64@gmail.com:user
+timwee:$2a$08$ytp5rBoSkcf20KPppavjWe8Jahw9JNjHZEK1/9dxiAfZew75tOJ.G:Tim Wee:timwee999@gmail.com:user
