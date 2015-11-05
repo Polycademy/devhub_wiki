48693128610dac1a779b929b8c535139557e4f98
Roger Qiu <roger.qiu@polycademy.com>
2015-11-05 23:05:00 +1100

    WIP Making changes and preparing for codemasterinstitute deployment

--- conf/users.auth.php
+++ conf/users.auth.php
@@ -9,9 +9,5 @@
 # login:passwordhash:Real Name:email:groups,comma,seperated
 
 
-mariafoo:$2a$08$lsesZL7v2JssTsF8taTK9.xGazDDnVTHlk81CKZxiQ5FXcXS.QKOW:Maria Foo:mariafoo@iinet.net.au:user
-mattmaher:$2a$08$y0aqzsdYSbXXfLaxje1JWuRloeJ./spbSx.lLokjCThHg6uRDar/S:Matthew Maher:matthew.alexander.maher@gmail.com:user
-phuonglantrinh:$2a$08$Z42YaUv0AyjySM2IaQIgB.njBscNsxGqN1QdVrMY.5X/pJdWbWKDy:Phuong Lan Trinh:phuonglan.trinh91@gmail.com:user
-ash:$2a$08$X7dw1TKHvEENBHsgL62L0e1MjeYfbPiFpYuCOKKotT2W73.1XA9ny:Ash Newport:chickensoup64@gmail.com:user
-timwee:$2a$08$ytp5rBoSkcf20KPppavjWe8Jahw9JNjHZEK1/9dxiAfZew75tOJ.G:Tim Wee:timwee999@gmail.com:user
 cmcdragonkai:$2a$08$mYz05ttUI8QVIBEc4HgynOWgLVOtQZdD3CNmifcaZ2fjDd7X4UFfW:Roger Qiu:roger.qiu@polycademy.com:admin,user
+codemasterinstitute:$2a$08$3hPVwjpLaoMaQ4mHnBZvke2L18I8r0MlL5Mhx3Px0yZE.O/0YKwnC:Codemaster Institute:accounts@codemasterinstitute.com:admin,user
